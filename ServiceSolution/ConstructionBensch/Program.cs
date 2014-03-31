1f344a559a210fbfdb13a65dff10005469e9b649
sweco-semthd <mattias.hedman@sweco.se>
2014-03-31 10:26:07 +0200

    NAmespace

--- ServiceSolution/ConstructionBensch/Program.cs
+++ ServiceSolution/ConstructionBensch/Program.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConstructionBensch
+namespace Ut
 {
     class Program
     {
         static void Main(string[] args)
         {
 
+            using (var serviceHost = new ServiceHost(typeof(GeoDataService), new Uri[] {new Uri("http://localhost:5678/" }))) 
+            {
+                
+            }
             Console.WriteLine("Master I am here to serve you!");
             Console.WriteLine("Press any key to exit...");
             Console.Read();
---
+++ ServiceSolution/GeoDataServiceLib/GeoDataService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Ut
+{
+    [ServiceBehavior(InstanceContextMode=InstanceContextMode.PerCall, ConcurrencyMode = ConcurrencyMode.Multiple)]
+    public class GeoDataService 
+        : IGeoDataService
+    {
+
+        string IGeoDataService.SayHello()
+        {
+            return "Hello";
+        }
+    }
+}
