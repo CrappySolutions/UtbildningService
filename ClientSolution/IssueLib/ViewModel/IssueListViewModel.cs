d5955432552710b7e52d804b98a69a3491a90bd9
sweco-semthd <mattias.hedman@sweco.se>
2014-04-02 12:03:37 +0200

    ListView

--- ClientSolution/IssueLib/ViewModel/IssueListViewModel.cs
+++ ClientSolution/IssueLib/ViewModel/IssueListViewModel.cs
@@ -1,15 +1,40 @@
 ﻿using GalaSoft.MvvmLight;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace IssueLib.ViewModel
 {
-    public class IssueListViewModel : ViewModelBase, IIssueListViewModel
+    public class IssueListViewModel : ObservableCollection<CommonLib.AwesomeService.IssueItem>, IIssueListViewModel
     {
-        
+        private readonly CommonLib.AwesomeService.IGeoDataService _service;
+        public IssueListViewModel(CommonLib.AwesomeService.IGeoDataService service)
+        {
+            _service = service;
+            Init();
+        }
+
+        private async void Init()
+        {
+            try
+            {
+                var issues = await _service.GetAllIssuesAsync();
+                foreach (var issue in issues)
+                {
+                    await System.Windows.Application.Current.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Background, new Action(() =>
+                    {
+                        Add(issue);
+                    }));
+                }
+            }
+            catch (Exception)
+            {
+                
+            }
+        }
     }
 
     public interface IIssueListViewModel
